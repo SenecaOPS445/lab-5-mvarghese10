7bcce4dd074f0f3ae3dd99e728b4492152569e59
Megodwin Varghese <mvarghese10@myseneca.ca>
2024-07-11 00:19:33 -0400

    Completed lab5a

---
+++ lab5a-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Megodwin Varghese
+
+
+import lab5a        
+file_name = 'data.txt'                             
+print(lab5a.read_file_string(file_name))
+# Will print 'Hello World\nThis is the second line\nThird line\nLast line\n'
+print(lab5a.read_file_list(file_name))
+# Will print ['Hello World', 'This is the second line', 'Third line', 'Last line']
